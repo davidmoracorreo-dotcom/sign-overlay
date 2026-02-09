40b7e9c2d12a568ae5ffa404da0b394a15b02fce
David Mora <davidmora.correo@gmail.com>
2026-02-09 07:08:10 +0100

    load avatar glb

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,20 +1,21 @@
 import { Canvas } from "@react-three/fiber";
+import { useGLTF, OrbitControls } from "@react-three/drei";
 
-function Box() {
-  return (
-    <mesh>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial />
-    </mesh>
-  );
+function Avatar() {
+  const { scene } = useGLTF("/avatar_adult.glb");
+  return <primitive object={scene} />;
 }
 
 export default function Scene() {
   return (
-    <Canvas style={{ width: "100vw", height: "100vh", background: "#000" }} camera={{ position: [0, 0, 3] }}>
-      <ambientLight intensity={0.6} />
+    <Canvas
+      style={{ width: "100vw", height: "100vh", background: "#000" }}
+      camera={{ position: [0, 1.6, 3] }}
+    >
+      <ambientLight intensity={0.8} />
       <directionalLight position={[3, 3, 3]} intensity={1} />
-      <Box />
+      <Avatar />
+      <OrbitControls />
     </Canvas>
   );
 }
